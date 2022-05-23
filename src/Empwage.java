127288878a92ceaa872507c9c7f28899b53744ea
Ashwini Rathod <rashwini510@gmail.com>
2022-05-23 21:13:18 +0530

    Added:parttime empwage

--- src/Empwage.java
+++ src/Empwage.java
@@ -1,29 +1,21 @@
 public class Empwage{
-    public static void main(String[] args) {
-        final int isPART_TIME = 1;
-        final int isFULL_TIME = 2;
-        final int Emp_Rate_Per_Hr = 20;
-        final int WORKING_DAYS = 20;
-
-        int totalWage = 0;
-        for (int day = 1; day <= WORKING_DAYS; day++)
-        {
-        int empCheck = (int) (Math.random() * 100) % 3;
-        int workingHours =0;
-        switch (empCheck)
-        {
-            case isFULL_TIME:
+        public static void main(String[] args) {
+            int isPART_TIME = 1;
+            int isFULL_TIME = 2;
+            int Emp_Rate_Per_Hr = 20;
+            int empcheck = (int) (Math.random() * 100) % 3;
+            int workingHours = 0;
+            if (empcheck == isFULL_TIME) {
+                System.out.println("Employee is Present Full time");
                 workingHours = 8;
-                break;
-            case isPART_TIME:
+            } else if (empcheck == isPART_TIME) {
+                System.out.println("Employee is Present Part time");
                 workingHours = 4;
-                break;
-            default:
+            } else
+
+                System.out.println("Employee is Absent");
+
+            int wage = workingHours * Emp_Rate_Per_Hr;
+            System.out.println("Employee Daily Wage is " + wage);
         }
-        int wage = workingHours * Emp_Rate_Per_Hr;
-        System.out.println("Day" + day + " wage is:" + wage);
-        totalWage +=wage;
     }
-        System.out.println("Total wage for a month is " + totalWage);
-    }
-}
