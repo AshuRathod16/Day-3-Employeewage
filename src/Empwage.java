4f9a2aa53b0ecd148e0cd656c208501e08060053
Ashwini Rathod <rashwini510@gmail.com>
2022-05-23 20:26:39 +0530

    Added:empwage by using switchcase

--- src/Empwage.java
+++ src/Empwage.java
@@ -1,21 +1,24 @@
 public class Empwage{
-        public static void main(String[] args) {
-            int isPART_TIME = 1;
-            int isFULL_TIME = 2;
-            int Emp_Rate_Per_Hr = 20;
-            int empcheck = (int) (Math.random() * 100) % 3;
-            int workingHours = 0;
-            if (empcheck == isFULL_TIME) {
+    public static void main(String[] args) {
+        final int isPART_TIME = 1;
+        final int isFULL_TIME = 2;
+        final int Emp_Rate_Per_Hr = 20;
+        int empCheck = (int) (Math.random() * 100) % 3;
+        int workingHours =0;
+        switch (empCheck)
+        {
+            case isFULL_TIME:
                 System.out.println("Employee is Present Full time");
                 workingHours = 8;
-            } else if (empcheck == isPART_TIME) {
+                break;
+            case isPART_TIME:
                 System.out.println("Employee is Present Part time");
                 workingHours = 4;
-            } else
-
+                break;
+            default:
                 System.out.println("Employee is Absent");
-
-            int wage = workingHours * Emp_Rate_Per_Hr;
-            System.out.println("Employee Daily Wage is " + wage);
-            }
-            }
+        }
+        int wage = workingHours * Emp_Rate_Per_Hr;
+        System.out.println("Employee Daily Wage is " + wage);
+    }
+}
